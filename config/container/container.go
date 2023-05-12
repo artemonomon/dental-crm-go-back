43f8ebe9bd1ee6650d77d1365d46dbe14b6288bc
Artem Dobriak <artem.art4932@gmail.com>
2023-05-12 15:08:28 +0300

    init commit

---
+++ config/container/container.go
@@ -0,0 +1,83 @@
+package container
+
+import (
+	"github.com/GrassBusinessLabs/eduprog-go-back/config"
+	"github.com/GrassBusinessLabs/eduprog-go-back/internal/app"
+	"github.com/GrassBusinessLabs/eduprog-go-back/internal/infra/database"
+	"github.com/GrassBusinessLabs/eduprog-go-back/internal/infra/http/controllers"
+	"github.com/GrassBusinessLabs/eduprog-go-back/internal/infra/http/middlewares"
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/upper/db/v4"
+	"github.com/upper/db/v4/adapter/postgresql"
+
+	"log"
+	"net/http"
+)
+
+type Container struct {
+	Middlewares
+	Services
+	Controllers
+}
+
+type Middlewares struct {
+	AuthMw func(http.Handler) http.Handler
+}
+
+type Services struct {
+	app.AuthService
+	app.UserService
+}
+
+type Controllers struct {
+	controllers.AuthController
+	controllers.UserController
+}
+
+func New(conf config.Configuration) Container {
+	tknAuth := jwtauth.New("HS256", []byte(conf.JwtSecret), nil)
+	sess := getDbSess(conf)
+
+	userRepository := database.NewUserRepository(sess)
+	sessionRepository := database.NewSessRepository(sess)
+
+	userService := app.NewUserService(userRepository)
+	authService := app.NewAuthService(sessionRepository, userService, conf, tknAuth)
+
+	authController := controllers.NewAuthController(authService, userService)
+	userController := controllers.NewUserController(userService)
+
+	authMiddleware := middlewares.AuthMiddleware(tknAuth, authService, userService)
+
+	return Container{
+		Middlewares: Middlewares{
+			AuthMw: authMiddleware,
+		},
+		Services: Services{
+			authService,
+			userService,
+		},
+		Controllers: Controllers{
+			authController,
+			userController,
+		},
+	}
+}
+
+func getDbSess(conf config.Configuration) db.Session {
+	sess, err := postgresql.Open(
+		postgresql.ConnectionURL{
+			User:     conf.DatabaseUser,
+			Host:     conf.DatabaseHost,
+			Password: conf.DatabasePassword,
+			Database: conf.DatabaseName,
+		})
+	//sess, err := sqlite.Open(
+	//	sqlite.ConnectionURL{
+	//		Database: conf.DatabasePath,
+	//	})
+	if err != nil {
+		log.Fatalf("Unable to create new DB session: %q\n", err)
+	}
+	return sess
+}
